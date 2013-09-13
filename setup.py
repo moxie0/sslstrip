3ac9db38e24524629e9ede9bda0ec82bd5c58829
Thomas Grainger <thomasgrainger@graingert.co.uk>
2013-09-13 13:52:59 +0100

    configure python packaging correctly

--- setup.py
+++ setup.py
@@ -1,56 +1,23 @@
 #!/usr/bin/env python
-import sys, os, shutil
-from distutils.core import setup, Extension
+from setuptools import setup, find_packages
 
-
-shutil.copyfile("sslstrip.py", "sslstrip/sslstrip")
-
-setup  (name        = 'sslstrip',
-        version     = '0.9',
-        description = 'A MITM tool that implements Moxie Marlinspike\'s HTTPS stripping attacks.',
-        author = 'Moxie Marlinspike',
-        author_email = 'moxie@thoughtcrime.org',
-        url = 'http://www.thoughtcrime.org/software/sslstrip/',
-        license = 'GPL',
-        packages  = ["sslstrip"],
-        package_dir = {'sslstrip' : 'sslstrip/'},
-        scripts = ['sslstrip/sslstrip'],
-        data_files = [('share/sslstrip', ['README', 'COPYING', 'lock.ico'])],
-       )
-
-print "Cleaning up..."
-try:
-    removeall("build/")
-    os.rmdir("build/")
-except:
-    pass
-
-try:
-    os.remove("sslstrip/sslstrip")
-except:
-    pass
-
-def capture(cmd):
-    return os.popen(cmd).read().strip()
-
-def removeall(path):
-	if not os.path.isdir(path):
-		return
-
-	files=os.listdir(path)
-
-	for x in files:
-		fullpath=os.path.join(path, x)
-		if os.path.isfile(fullpath):
-			f=os.remove
-			rmgeneric(fullpath, f)
-		elif os.path.isdir(fullpath):
-			removeall(fullpath)
-			f=os.rmdir
-			rmgeneric(fullpath, f)
-
-def rmgeneric(path, __func__):
-	try:
-		__func__(path)
-	except OSError, (errno, strerror):
-		pass
+setup  (
+    name        = 'sslstrip',
+    version     = '0.9.2',
+    description = 'A MITM tool that implements Moxie Marlinspike\'s HTTPS stripping attacks.',
+    author = 'Moxie Marlinspike',
+    author_email = 'moxie@thoughtcrime.org',
+    url = 'https://github.com/graingert/sslstrip/',
+    license = 'GPL',
+    packages  =  find_packages('src'),
+    package_dir = {'' : 'src'},
+    entry_points = {
+        'console_scripts': [
+            'sslstrip = sslstrip.sslstrip:_main',
+        ],
+    },
+    install_requires = [
+        'Twisted==13.1.0',
+        'pyOpenSSL==0.13.1',
+    ]
+)
